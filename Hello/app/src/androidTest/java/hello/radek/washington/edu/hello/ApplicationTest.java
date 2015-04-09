1be2c293ff2ebbda4f6fdde0ea59f78fad53abae
Yuefeng Li <radek@uw.edu>
2015-04-08 18:05:39 -0700

    HW2

---
+++ Hello/app/src/androidTest/java/hello/radek/washington/edu/hello/ApplicationTest.java
@@ -0,0 +1,13 @@
+package hello.radek.washington.edu.hello;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
